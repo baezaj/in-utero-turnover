2937091977b4e2f1c52b95fe84b3607a113a3803
baezaj <baeza@pennmedicine.upenn.edu>
2021-04-16 12:24:30 -0400

    refactoring code

--- Figure4/Fig4_01_Peptide_processing.R
+++ Figure4/Fig4_01_Peptide_processing.R
@@ -1,66 +1,68 @@
 
 # libraries ---------------------------------------------------------------
 
-
 library(tidyverse)
 library(rio)
-library(ggthemes)
-library(broom)
+library(janitor)
 
 
 # data import -------------------------------------------------------------
 
-
-data <- import("data/20180811_Mouse_Fetal_Development_Liver-(1)_PeptideGroups.txt")
-input_files <- import("data/20180811_Mouse_Fetal_Development_Liver-(1)_InputFiles.txt")
+# Liver data
+data_liver <- import("data/20180811_Mouse_Fetal_Development_Liver-(1)_PeptideGroups.txt")
+input_files_liver <- import("data/20180811_Mouse_Fetal_Development_Liver-(1)_InputFiles.txt")
 sample_list <- import("data/20180717_Sample_prep.xlsx")
-# mida_median <- import("processed_data/20181120_MIDA_analysis_summary.csv")
+
+# Lung data
+data_lung <- import("data/20190621_Lung_Translatome_PeptideGroups.txt", setclass = "tibble")
+input_files_lung <- import("data/20190621_Lung_Translatome_InputFiles.txt")
 
 
-# Formatting Input file ---------------------------------------------------
+# Liver data --------------------------------------------------------------
 
+##############################
+### Formatting input files ###
+##############################
 
 # Input file names
-input_files <- input_files %>%
-  rename_all(tolower) %>% 
-  rename_all(~str_replace_all(., "\\W", "_"))
-
+input_files_liver <- input_files_liver %>%
+  clean_names()
 
 # converting to NA
-input_files[][input_files[] == ""] <- NA
+input_files_liver[][input_files_liver[] == ""] <- NA
 
 # Subsetting columns and rows
-input_files <- input_files %>% 
+input_files_liver <- input_files_liver %>% 
   filter(!is.na(study_file_id)) %>% 
   select(study_file_id, file_name) %>% 
   mutate(study_file_id = tolower(study_file_id)) %>% 
   rename(file_id = "study_file_id")
 
 # Formatting rawfile name
-input_files$file_name <- unlist(lapply(input_files$file_name, function(x){
+input_files_liver$file_name <- unlist(lapply(input_files_liver$file_name, function(x){
   unlist(strsplit(x, split = "\\", fixed = TRUE))[9]
 }))
-input_files$file_name <- unlist(lapply(input_files$file_name, function(x){
+input_files_liver$file_name <- unlist(lapply(input_files_liver$file_name, function(x){
   unlist(strsplit(x, split = ".", fixed = TRUE))[1]
 }))
 
-input_files <- input_files %>% 
+input_files_liver <- input_files_liver %>% 
   separate(file_name, c("temp1", "temp2", "temp3", "temp4", "fetus_id"), remove = FALSE, convert = TRUE) %>% 
   select(-contains("temp")) 
 
-input_files$fetus_id[input_files$file_name == "20180811_E13_Liver_2_126"] <- 39
-input_files$fetus_id[input_files$file_name == "20180811_E13_Liver_4_130"] <- 46
-input_files$fetus_id[input_files$file_name == "20180811_E13_Liver_6_138"] <- 54
-input_files$fetus_id[input_files$file_name == "20180811_E14_Liver_2_39"] <- 68
-input_files$fetus_id[input_files$file_name == "20180811_E14_Liver_4_46"] <- 77
-input_files$fetus_id[input_files$file_name == "20180811_E14_Liver_6_54"] <- 86
-input_files$fetus_id[input_files$file_name == "20180811_E16_Liver_2_68"] <- 126
-input_files$fetus_id[input_files$file_name == "20180811_E16_Liver_4_77"] <- 130
-input_files$fetus_id[input_files$file_name == "20180811_E16_Liver_6_86"] <- 138
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E13_Liver_2_126"] <- 39
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E13_Liver_4_130"] <- 46
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E13_Liver_6_138"] <- 54
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E14_Liver_2_39"] <- 68
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E14_Liver_4_46"] <- 77
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E14_Liver_6_54"] <- 86
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E16_Liver_2_68"] <- 126
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E16_Liver_4_77"] <- 130
+input_files_liver$fetus_id[input_files_liver$file_name == "20180811_E16_Liver_6_86"] <- 138
 
 
-# Formatting sample list --------------------------------------------------
 
+### Formatting sample list ###
 
 sample_list <- sample_list %>% 
   select(fetus_id, sample_name, batch)
@@ -76,17 +78,17 @@ sample_list$fetus_id[sample_list$sample_name == "Batch_6"] <- 6
 
 
 # Merging with input files
-input_files <- right_join(sample_list, input_files) %>% 
+input_files_liver <- right_join(sample_list, input_files_liver) %>% 
   select(-file_name)
 
-input_files$sample_name[input_files$sample_name == "Batch_1"] <- "Batch_Liver_0hr_1"
-input_files$sample_name[input_files$sample_name == "Batch_2"] <- "Batch_Liver_0hr_2"
-input_files$sample_name[input_files$sample_name == "Batch_3"] <- "Batch_Liver_0hr_3"
-input_files$sample_name[input_files$sample_name == "Batch_4"] <- "Batch_Liver_0hr_4"
+input_files_liver$sample_name[input_files_liver$sample_name == "Batch_1"] <- "Batch_Liver_0hr_1"
+input_files_liver$sample_name[input_files_liver$sample_name == "Batch_2"] <- "Batch_Liver_0hr_2"
+input_files_liver$sample_name[input_files_liver$sample_name == "Batch_3"] <- "Batch_Liver_0hr_3"
+input_files_liver$sample_name[input_files_liver$sample_name == "Batch_4"] <- "Batch_Liver_0hr_4"
 
 
 # Cleaning input file names
-input_files <- input_files %>% 
+input_files_liver <- input_files_liver %>% 
   separate(sample_name, c("dev_stage", "tissue", "time", "temp"), remove = FALSE, convert = TRUE) %>% 
   mutate(time = as.numeric(str_sub(time, start = 1L, end = -3L)),
          sample_name = str_replace_all(sample_name, "hr", "")) %>% 
@@ -95,47 +97,47 @@ input_files <- input_files %>%
 # removing sample list
 rm(sample_list);gc()
 
+#######################################
+#### Extracting peptide abundances ####
+#######################################
 
-# Peptide abundance -------------------------------------------------------
 
 # formatting names and selecting columns for use
-data <- data %>% 
-  rename_all(tolower) %>% 
-  rename_all(~str_replace_all(., "\\W", "_")) %>% 
-  rename_all(~str_replace_all(., "___", "_")) %>% 
-  rename_all(~str_replace_all(., "__", "_")) %>% 
+data_liver <- data_liver %>% 
+  clean_names() %>% 
   select(-contains("found_in_"), 
          -contains("ratio"), 
          -contains("abundances_"))
 
-data$master_protein_descriptions <- gsub(" \\[OS=Mus musculus]", "", data$master_protein_descriptions)
+# Cleaning protein description
+data_liver$master_protein_descriptions <- gsub(" \\[OS=Mus musculus]", "", data_liver$master_protein_descriptions)
 
 # data wrangling
-data_tidy <- data %>% 
+liver_tidy <- data_liver %>% 
   filter(contaminant == FALSE) %>% 
-  select(sequence, modifications, theo_mhplus_in_da,
+  select(sequence, modifications, theo_m_hplus_in_da,
          master_protein_accessions, master_protein_descriptions, 
          contains("abundance_")) %>% 
   gather(temp, value, contains("abundance_")) %>% 
   separate(temp, c("temp1", "file_id", "isotope", "temp4", "temp5", "temp6", "temp7", "temp8"), sep = "_") %>% 
-  right_join(input_files, .) %>%
+  right_join(input_files_liver, .) %>%
   mutate(value = log2(value)) %>% 
   select(-contains("temp"), -file_id)
 
 # Metadata
-data <- data %>% 
+data_liver <- data_liver %>% 
   filter(contaminant == "FALSE") %>% 
   select(-contains("abundance_"))
 
 # Removing tables
-rm(input_files);gc()
+rm(input_files_liver);gc()
 
-
-# Generating model input data ---------------------------------------------
-
+#########################################
+#### Generating the model input data ####
+#########################################
 
 # calculating fraction labeled
-data_ria <- data_tidy %>% 
+liver_ria <- liver_tidy %>% 
   filter(!is.na(value)) %>% 
   spread(isotope, value) %>% 
   mutate(total = ifelse(is.na(heavy) & !is.na(light), light, log2(2^heavy + 2^light)),
@@ -145,9 +147,8 @@ data_ria <- data_tidy %>%
          light = 2^light,
          total = 2^total)
 
-
 # Adding a time point of zero
-data_temp <- data_ria %>% 
+data_temp <- liver_ria %>% 
   select(dev_stage, fetus_id, tissue, time, batch) %>% 
   mutate(time = 0) %>% 
   distinct(dev_stage, tissue, time, batch, .keep_all = TRUE) %>% 
@@ -156,10 +157,131 @@ data_temp <- data_ria %>%
   filter(dev_stage != "Batch")
 
 # creating a dataframe for the zero timepoint
-data_zero_tp <- data_ria %>% 
+data_zero_tp <- liver_ria %>% 
   arrange(time) %>% 
   select(-dev_stage, -fetus_id, -batch, -sample_name) %>% 
-  distinct(tissue, sequence, modifications, theo_mhplus_in_da, 
+  distinct(tissue, sequence, modifications, theo_m_hplus_in_da, 
+           master_protein_accessions, master_protein_descriptions, .keep_all = TRUE) %>% 
+  mutate(time = 0,
+         heavy = 0,
+         light = NA,
+         total = NA,
+         fraction = 0,
+         has_fraction = TRUE) %>% 
+  right_join(data_temp, .)
+
+all(names(liver_ria) %in% names(data_zero_tp))
+
+# combining the zero time point with the data
+liver_nls <- bind_rows(liver_ria, data_zero_tp) %>% 
+  filter(!is.na(fraction))
+
+# removing tables
+rm(data_temp, data_zero_tp, liver_ria);gc()
+
+
+
+# Lung data processing ----------------------------------------------------
+
+##############################
+### Formatting input files ###
+##############################
+
+# Input file names
+input_files_lung <- input_files_lung %>%
+  clean_names()
+
+# converting to NA
+input_files_lung[][input_files_lung[] == ""] <- NA
+
+# Subsetting columns and rows
+input_files_lung <- input_files_lung %>% 
+  filter(!is.na(file_id)) %>% 
+  select(file_id, file_name) %>% 
+  mutate(file_id = tolower(file_id)) 
+
+# Formatting rawfile name
+input_files_lung$file_name <- unlist(lapply(input_files_lung$file_name, function(x){
+  unlist(strsplit(x, split = "\\", fixed = TRUE))[9]
+}))
+input_files_lung$file_name <- unlist(lapply(input_files_lung$file_name, function(x){
+  unlist(strsplit(x, split = ".", fixed = TRUE))[1]
+}))
+
+input_files_lung <- input_files_lung %>% 
+  separate(file_name, c("temp1", "dev_stage", "tissue",
+                        "time", "fetus_id", "batch"),
+           remove = FALSE, convert = TRUE) %>% 
+  select(-contains("temp")) %>% 
+  unite(sample_name, dev_stage, tissue, time, fetus_id, sep = "_", remove = FALSE) %>% 
+  mutate(batch = batch + 4)
+
+#######################################
+#### Extracting peptide abundances ####
+#######################################
+
+# formatting names and selecting columns for use
+data_lung <- data_lung %>% 
+  clean_names() %>% 
+  select(-contains("found_in_"), 
+         -contains("ratio"), 
+         -contains("abundances_"))
+
+# Cleaning up protein description
+data_lung$master_protein_descriptions <- gsub(" \\[OS=Mus musculus]", "", data_lung$master_protein_descriptions)
+
+
+# data wrangling
+lung_tidy <- data_lung %>% 
+  filter(contaminant == FALSE) %>% 
+  select(sequence, modifications, theo_m_hplus_in_da,
+         master_protein_accessions, master_protein_descriptions, 
+         contains("abundance_")) %>% 
+  gather(temp, value, contains("abundance_")) %>% 
+  separate(temp, c("temp1", "file_id", "isotope", 
+                   "temp4", "temp5", "temp6", "temp7"), sep = "_") %>% 
+  right_join(input_files_lung, .) %>%
+  mutate(value = log2(value)) %>% 
+  select(-contains("temp"), -file_id, -file_name)
+
+# Metadata
+data_lung <- data_lung %>% 
+  filter(contaminant == "FALSE") %>% 
+  select(-contains("abundance_"))
+
+# Removing tables
+rm(input_files_lung);gc()
+
+#########################################
+#### Generating the model input data ####
+#########################################
+
+# calculating fraction labeled
+lung_ria <- lung_tidy %>% 
+  filter(!is.na(value)) %>% 
+  spread(isotope, value) %>% 
+  mutate(total = ifelse(is.na(heavy) & !is.na(light), light, log2(2^heavy + 2^light)),
+         fraction = ifelse(is.na(heavy), NA, (2^heavy / 2^total)),
+         has_fraction = ifelse(is.na(heavy), FALSE, TRUE)) %>% 
+  mutate(heavy = 2^heavy,
+         light = 2^light,
+         total = 2^total)
+
+# Adding a time point of zero
+data_temp <- lung_ria %>% 
+  select(dev_stage, fetus_id, tissue, time, batch) %>% 
+  mutate(time = 0) %>% 
+  distinct(dev_stage, tissue, time, batch, .keep_all = TRUE) %>% 
+  mutate(fetus_id = batch) %>%
+  unite(sample_name, dev_stage, tissue, time, fetus_id, sep = "_", remove = FALSE) %>% 
+  filter(dev_stage != "Std")
+
+# creating a dataframe for the zero timepoint
+data_zero_tp <- lung_ria %>% 
+  filter(tissue == "Lung") %>% 
+  arrange(time) %>% 
+  select(-dev_stage, -fetus_id, -batch, -sample_name) %>% 
+  distinct(tissue, sequence, modifications, theo_m_hplus_in_da, 
            master_protein_accessions, master_protein_descriptions, .keep_all = TRUE) %>% 
   mutate(time = 0,
          heavy = 0,
@@ -172,15 +294,39 @@ data_zero_tp <- data_ria %>%
 all(names(data_ria) %in% names(data_zero_tp))
 
 # combining the zero time point with the data
-data_lm <- bind_rows(data_ria, data_zero_tp) %>% 
+lung_nls <- bind_rows(lung_ria, data_zero_tp) %>% 
   filter(!is.na(fraction))
 
 # removing tables
-rm(data_temp, data_zero_tp);gc()
+rm(data_temp, data_zero_tp, lung_ria);gc()
+
+
+# Merging datasets --------------------------------------------------------
+
+
+# Combining both datasets
+data_tidy <- bind_rows(liver_tidy, lung_tidy)
+
+# Cleaning up names
+data_tidy <- data_tidy %>% 
+  mutate(dev_stage = str_replace_all(dev_stage, "Batch", "Std")) %>% 
+  unite(sample_name, dev_stage, tissue, time, batch, fetus_id, remove = FALSE) %>% 
+  filter(!is.na(value))
+
+
+# Combining both datasets
+data_nls <- bind_rows(liver_nls, lung_nls)
+
+# Cleaning up names
+data_nls <- data_nls %>% 
+  mutate(dev_stage = str_replace_all(dev_stage, "Batch", "Std")) %>% 
+  unite(sample_name, dev_stage, tissue, time, batch, fetus_id, remove = FALSE) %>% 
+  filter(dev_stage != "Std")
 
 # Data export -------------------------------------------------------------
 
-export(data_tidy, file = "intermediate_data/02_out_Liver_Peptide_tidy_data.csv")
-export(data, file = "intermediate_data/02_out_Liver_Peptide_metadata.csv")
-export(data_lm, file = "intermediate_data/02_out_Liver_Peptide_modeling_input_data.csv")
+export(data_tidy, file = "intermediate_data/01_out_Liver_Lung_combined_Peptide_tidy_data.csv")
+export(data_nls, file = "intermediate_data/01_out_Liver_Lung_combined_Peptide_modeling_input_data.csv")
+
+
 
--- Figure4/Fig4_02_Peptide_modeling_nls.R
+++ Figure4/Fig4_02_Peptide_modeling_nls.R
@@ -1,35 +1,35 @@
 
-# libraries ---------------------------------------------------------------
-
+# Document Setup ----------------------------------------------------------
 
 library(tidyverse)
 library(rio)
 library(broom)
 library(modelr)
-library(ggthemes)
+
+# Data Import -------------------------------------------------------------
 
 
-# data import -------------------------------------------------------------
+data_nls <- import("intermediate_data/01_out_Liver_Lung_combined_Peptide_modeling_input_data.csv", setclass = "tibble")
 
-
-data_lm <- import("intermediate_data/02_out_Liver_Peptide_modeling_input_data.csv")
 
 
 # Formatting --------------------------------------------------------------
 
+
 # calculating fraction_RIA
-data_lm <- data_lm %>% 
+data_nls <- data_nls %>% 
+  filter(!is.na(fraction)) %>% 
   select(-heavy, -total, -light, -has_fraction)
 
 # Filtering peptides seen in all time points
-temp <- data_lm %>% 
+temp <- data_nls %>% 
   filter(time > 0) %>%
-  select(sequence, modifications, theo_mhplus_in_da, 
+  select(tissue, sequence, modifications, theo_m_hplus_in_da, 
          master_protein_accessions, master_protein_descriptions) %>% 
   distinct() %>% 
   ungroup()
 
-data_lm <- inner_join(data_lm, temp)
+data_nls <- inner_join(data_nls, temp)
 rm(temp);gc()
 
 
@@ -44,17 +44,16 @@ nls_pulse_chase <- function(data, x = time, y = fraction){
     nls(fraction ~ exp(-k*time)*(k*time),
         data = data,
         start = list(k = start_val),
-        control = nls.control(maxiter = 100)),
+        control = nls.control(maxiter = 1000)),
     error = function(e) NA, 
     warning = function(w) NA
   )
   
 }
 
-
 # Function that creates a dataframe with model predictions
 best_fit_line <- function(data){
-  data.frame(time = seq(0, 6, 0.1)) %>% 
+  data.frame(time = seq(0, 6, 0.2)) %>% 
     add_predictions(data)
 }
 
@@ -65,41 +64,42 @@ time_count <- function(x){
 }
 
 
-
 # Modeling ----------------------------------------------------------------
+
 
 # Nesting the data
 system.time(
-  data_nest <- data_lm %>% 
-    filter(dev_stage != "Batch") %>% 
-    group_by(dev_stage, tissue, sequence, modifications, theo_mhplus_in_da,
+  data_nest <- data_nls %>% 
+    group_by(tissue, dev_stage, sequence, modifications, theo_m_hplus_in_da,
              master_protein_accessions, master_protein_descriptions) %>% 
     nest() %>% 
     mutate(n_tp = map_dbl(data, time_count)) %>% 
     filter(n_tp == 4) %>% 
     mutate(n_runs = map_dbl(data, nrow))
 )
+# Time to complete - 5.8 sec
 
 ggplot(data_nest) +
   geom_bar(aes(x = n_runs))
 
-
 # Performing the models
 system.time(
   data_nest <- data_nest %>% 
-    filter(n_runs >= 9) %>% 
+    filter(n_runs >= 8) %>% 
     mutate(model_pc = map(data, nls_pulse_chase))
 )
-
+# Time to complete - 67 sec
 
 # Model summary statistics ------------------------------------------------
 
 
 # Tidying the data
 model_tidy <- data_nest %>% 
-  filter(model_pc != "NA") %>% 
+  filter(model_pc != "NA") %>%
   mutate(tidy_nls = map(model_pc, tidy)) %>%
   unnest(tidy_nls) %>%
+  mutate(model = "pulse_chase") %>% 
+  arrange(-n_runs, sequence)%>% 
   select(-data, -model_pc)
 
 
@@ -108,20 +108,12 @@ model_tidy <- data_nest %>%
 
 # Glancing the model
 model_glance <- data_nest %>%
-  filter(model_pc != "NA") %>% 
+  filter(model_pc != "NA") %>%
   mutate(glance_nls = map(model_pc, glance)) %>%
   unnest(glance_nls) %>%
-  select(-data, -model_pc)
-
-
-# Augmenting --------------------------------------------------------------
-
-# Glancing the model
-model_augment <- data_nest %>%
-  filter(model_pc != "NA") %>% 
-  mutate(augment_nls = map(model_pc, augment)) %>%
-  unnest(augment_nls) %>%
-  select(-data, -model_pc)
+  mutate(model = "pulse_chase") %>% 
+  select(-data, -model_pc) %>%
+  arrange(-n_runs, sequence)
 
 
 # Model Prediction - Best fit line ----------------------------------------
@@ -138,8 +130,6 @@ data_best_fit_line <- data_nest %>%
 # Data Export -------------------------------------------------------------
 
 
-export(model_glance, file = "intermediate_data/03_out_model_nls_peptide_fraction_broom_glance.csv")
-export(model_tidy, file = "intermediate_data/03_out_model_nls_peptide_fraction_broom_tidy.csv")
-export(data_best_fit_line, file = "intermediate_data/03_out_nls_model_BestFitLine_data.csv")
-
-
+export(model_glance, file = "intermediate_data/02_out_model_nls_peptide_fraction_broom_glance.csv")
+export(model_tidy, file = "intermediate_data/02_out_model_nls_peptide_fraction_broom_tidy.csv")
+export(data_best_fit_line, file = "intermediate_data/02_out_nls_model_BestFitLine_data.csv")
