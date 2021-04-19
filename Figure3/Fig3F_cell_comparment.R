a54b85ca96fcdee2bdb308ae1d569a926be1d57b
baezaj <baeza@pennmedicine.upenn.edu>
2021-04-19 19:42:37 -0400

    refactoring figure 3F

--- Figure3/Fig3F_cell_comparment.R
+++ Figure3/Fig3F_cell_comparment.R
@@ -1,15 +1,14 @@
 
-# setup -------------------------------------------------------------------
+# libraries ---------------------------------------------------------------
 
 
 library(tidyverse)
 library(rio)
 library(janitor)
 library(broom)
-library(purrr)
-library(modelr)
-library(ggthemes)
-library(forcats)
+# library(modelr)
+# library(ggthemes)
+# library(forcats)
 library(viridisLite)
 
 # data import -------------------------------------------------------------
@@ -18,6 +17,7 @@ library(viridisLite)
 model_tidy <- import("intermediate_data/Fig03_nls_model_peptide_fraction_broom_tidy.csv")
 proteins <- import("data/20171221_Baeza_DevAtlas_Brain_Liver_timecourse_Proteins.txt")
 input_files <- import("data/20171221_Baeza_DevAtlas_Brain_Liver_timecourse_InputFiles.txt")
+
 
 
 # formatting --------------------------------------------------------------
@@ -35,65 +35,70 @@ data_filtered <- model_tidy %>%
   mutate(t_half = log(2)/estimate) %>% 
   arrange(t_half)
 
+# Selecting first protein group
+data_filtered$master_protein_accessions <- unlist(lapply(data_filtered$master_protein_accessions, function(x){
+  unlist(strsplit(x, split = ";"))[1]
+}))
 
-input_files <- input_files %>% 
-  clean_names() %>% 
-  select(file_id, file_name) %>% 
+# Formatting protein data -------------------------------------------------
+
+# cleaning input file names
+input_files <- input_files %>%
+  clean_names() %>%
+  select(file_id, file_name) %>%
   mutate(file_id = tolower(file_id))
 
+# extracting raw file name
 input_files$file_name <- unlist(lapply(input_files$file_name, function(x){
   unlist(strsplit(x, split = "\\", fixed = TRUE))[8]
 }))
 
+# extracting tissue info
 input_files$tissue <- unlist(lapply(input_files$file_name, function(x){
   unlist(strsplit(x, split = "_", fixed = TRUE))[4]
 }))
 
-input_files <- input_files %>% 
+# selecting columns
+input_files <- input_files %>%
   select(file_id, tissue)
 
-prot_long <- proteins %>% 
-  clean_names() %>% 
+# tidying the protein data
+prot_long <- proteins %>%
+  clean_names() %>%
   select(-contains("found_"),
          -contains("abundances_"),
          -contains("abundance_r"),
          -contains("search_engine"),
-         # -contains("number_of"),
+         -contains("number_of"),
          -contains("sequence"),
-         -contains("pathway")) %>% 
-  gather(temp, value, contains("abundance")) %>% 
-  separate(temp, into = c("temp1", "file_id", "isotope", "temp2"), sep = "_") %>% 
-  select(-contains("temp")) %>% 
-  left_join(., input_files) %>% 
-  spread(isotope, value) %>% 
+         -contains("pathway")) %>%
+  gather(temp, value, contains("abundance")) %>%
+  separate(temp, into = c("temp1", "file_id", "isotope", "temp2"), sep = "_") %>%
+  select(-contains("temp")) %>%
+  left_join(., input_files) %>%
+  spread(isotope, value) %>%
   mutate(abundance = ifelse(!is.na(heavy) & !is.na(light), heavy + light, light))
 
-prot_long <- prot_long %>% 
-  filter(!is.na(heavy) | !is.na(light))
-
-prot_long <- prot_long %>% 
-  group_by(accession, tissue, mw_in_k_da, coverage_in_percent, number_of_peptides, number_of_ps_ms,
-           biological_process, molecular_function, cellular_component) %>% 
-  summarize(abundance_median = median(abundance, na.rm = TRUE)) %>% 
-  ungroup()
-
-prot_long <- prot_long %>% 
+# filtering for proteins with heavy and light quant values
+# Summarizing protein abundance
+# using median value
+prot_long <- prot_long %>%
+  filter(!is.na(heavy) | !is.na(light))%>%
+  group_by(accession, tissue, mw_in_k_da, coverage_in_percent,
+           biological_process, molecular_function, cellular_component) %>%
+  summarize(abundance_median = median(abundance, na.rm = TRUE)) %>%
+  ungroup() %>%
   rename(master_protein_accessions = "accession")
 
-prot_go <- prot_long %>% 
+# selecting gene ontology data
+# in reality, only interested in cell compartment
+prot_go <- prot_long%>% 
   gather(go, name, biological_process, molecular_function, cellular_component) %>% 
   filter(name != "") %>% 
   mutate(name = strsplit(name, split = ";")) %>% 
   unnest(name)
 
-
-data_filtered$master_protein_accessions <- unlist(lapply(data_filtered$master_protein_accessions, function(x){
-  unlist(strsplit(x, split = ";"))[1]
-}))
-
-
-
-
+# Merging protein data with turnover rates
 data_filtered <- left_join(data_filtered, prot_go) %>% 
   filter(!is.na(abundance_median))
 
@@ -103,6 +108,7 @@ all <- data_filtered %>%
   distinct() %>% 
   mutate(name = "total")
 
+# merging the total with the dataframe
 data_filtered <- bind_rows(data_filtered, all)
 
 
@@ -110,12 +116,10 @@ data_filtered <- bind_rows(data_filtered, all)
 # plots -------------------------------------------------------------------
 
 
-
-
+# turnover rates across cell compartments
 ggplot(data_filtered %>% filter(go == "cellular_component", 
                                 name != "vacuole"),
        aes(x = name, y = log10(estimate))) +
-  # aes(x = fct_reorder(name, name, na.rm = TRUE), y = log10(estimate))) +
   geom_jitter(width = 0.2, alpha = 0.01) +
   geom_boxplot(alpha = 0) +
   theme_bw(base_size = 14) +
@@ -123,9 +127,6 @@ ggplot(data_filtered %>% filter(go == "cellular_component",
   coord_flip() +
   labs(y = "synthesis rate Log10",
        x = "Cellular Compartment")
-
-
-
 
 
 # Stats -------------------------------------------------------------------
@@ -148,7 +149,7 @@ stats_brain <- tidy(TukeyHSD(aov(log10(estimate) ~ name,
                                  data = cc_brain,
                                  na.action = na.exclude)))
 
-
+# data for liver
 liv_plot <- stats_liver %>% 
   separate(contrast, into = c("cc1", "cc2"), sep = "-") %>% 
   mutate(adj.p.value = ifelse(adj.p.value > 0.05, NA, adj.p.value))
@@ -160,8 +161,8 @@ ggplot(liv_plot) +
   scale_fill_gradientn(colors = viridis(100, option = "D", direction = 1)) +
   labs(title = "Liver",
        fill = "-Log10(q.value)")
-# ggsave(filename = "figures/20201027_liver_stats.pdf")
 
+# data from brain
 br_plot <- stats_brain %>% 
   separate(contrast, into = c("cc1", "cc2"), sep = "-") %>% 
   mutate(adj.p.value = ifelse(adj.p.value > 0.05, NA, adj.p.value))
@@ -174,4 +175,4 @@ ggplot(br_plot) +
   labs(title = "Brain",
        fill = "-Log10(q.value)") 
 
-# ggsave(filename = "figures/20201027_brain_stats.pdf")
+
