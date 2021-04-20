0217ff9b621e1ac2e2c0182071ba99fcb1914d85
baezaj <baeza@pennmedicine.upenn.edu>
2021-04-20 08:37:18 -0400

    Refactoring Figure 4

--- Figure4/Fig4_04_D-F_SuppFig5.R
+++ Figure4/Fig4_04_D-F_SuppFig5.R
@@ -5,17 +5,12 @@
 library(tidyverse)
 library(rio)
 library(broom)
-library(patchwork)
 library(UpSetR)
 
 
 # data import -------------------------------------------------------------
 
-
 model_tidy <- import("intermediate_data/Fig04_nls_model_peptide_fraction_broom_tidy.csv")
-model_glance <- import("intermediate_data/Fig04_nls_model_peptide_fraction_broom_glance.csv")
-bfl <- import("intermediate_data/Fig04_nls_model_BestFitLine_data.csv")
-data_nls <- import("intermediate_data/Fig04_Liver_Lung_combined_Peptide_modeling_input_data.csv")
 
 
 # Filtering poor model fits -----------------------------------------------
@@ -32,21 +27,6 @@ model_tidy <- model_tidy %>%
          KP_count = str_count(sequence, "KP"),
          missed_cleavage = R_count + K_count - RP_count - KP_count - 1) %>% 
   filter(missed_cleavage < 2)
-
-
-temp <- model_tidy %>% 
-  select(dev_stage:n_runs)
-
-# removing poor model fits from glance data
-model_glance <- inner_join(model_glance, temp) %>% 
-  unite(id, tissue, dev_stage, sep = "-", remove = FALSE)
-
-# removing poor model fits from glance data
-bfl <- inner_join(bfl, temp)%>% 
-  unite(id, tissue, dev_stage, sequence, modifications, theo_m_hplus_in_da, sep = "_", remove = FALSE)
-
-
-rm(temp);gc()
 
 
 # Functions ---------------------------------------------------------------
@@ -80,7 +60,6 @@ data_nest <- model_tidy %>%
   group_by(tissue, peptide_id) %>% 
   mutate(id_n = n()) %>% 
   ungroup() %>% 
-  # filter(id_n >= 3) %>% 
   mutate(gestation = dev_stage, 
          dev_stage = str_remove_all(dev_stage, "E"),
          dev_stage = str_replace_all(dev_stage, "P0", "20"),
@@ -88,7 +67,6 @@ data_nest <- model_tidy %>%
   group_by(tissue, master_protein_accessions, master_protein_descriptions) %>% 
   nest() %>% 
   mutate(devstage_n = map_dbl(data, count_gestation),
-         # tissue_n = map_dbl(data, count_tissue),
          peptide_n = map_dbl(data, count_peptides))
 
 # Running the model
@@ -131,35 +109,6 @@ ggplot(hemoglobin %>% filter(id_n >= 4, tissue == "Liver", master_protein_access
   scale_y_log10()
 
 
-
-# Supp Fig 5A -------------------------------------------------------------
-
-
-# Turnover rate plots
-ggplot(bfl) +
-  geom_line(aes(x = time, y = pred, group = id), alpha = 0.02) +
-  facet_grid(vars(tissue), vars(dev_stage)) +
-  theme_light(base_size = 14) +
-  labs(y = "Fraction",
-       x = "Time (hours)")
-
-
-# Supp Fig 5B -------------------------------------------------------------
-
-liv_dat <- data_nls %>% 
-  filter(master_protein_accessions %in% liv_list, tissue == "Liver",
-         sequence != "IGGHGAEYGAEALER") %>% 
-  mutate(mox = str_detect(modifications, "(?i)oxidation")) %>% 
-  filter(mox == FALSE)
-
-# Turnover rate profiles for hemoglobin proteins
-ggplot(bfl %>% filter(master_protein_accessions %in% liv_list, tissue == "Liver"),
-       aes(x = time, color = sequence)) +
-  geom_line(aes(y = pred, group = id)) +
-  geom_jitter(data = liv_dat, aes(y = fraction), width = 0.05) +
-  facet_grid(vars(master_protein_descriptions), vars(dev_stage), scales = "free_y") +
-  guides(color = FALSE) +
-  theme_light(base_size = 10)
 
 
 # Fig4 E ------------------------------------------------------------------
@@ -250,121 +199,6 @@ ggplot(ribosomal %>% filter(master_protein_accessions %in% ribo_lung$protein, pe
   theme_bw(base_size = 10) +
   facet_wrap(~master_protein_descriptions, nrow = 1, labeller = label_wrap_gen(30)) +
   scale_y_log10()
-
-
-
-
-# Supp Fig 5C -------------------------------------------------------------
-
-
-ribo_liver_lung <- ribo_prot %>% 
-  filter(`Liver-E13` == 1,
-         `Liver-E14` == 1,
-         `Liver-E16` == 1,
-         `Liver-P0` == 1,
-         `Lung-E13` == 1,
-         `Lung-E14` == 1,
-         `Lung-E16` == 1,
-         `Lung-P0` == 1)
-
-# Boxplot of ribosomal proteins
-# Detected in liver and lung
-ggplot(ribosomal %>% filter(master_protein_accessions %in% ribo_liver_lung$protein, peptide_n >= 2),
-       aes(x = gestation, y = estimate, color = tissue)) +
-  geom_boxplot(outlier.color = NA, size = 0.25) +
-  geom_point(position = position_jitterdodge(seed = 1), size = 0.4) +
-  theme_bw(base_size = 10) +
-  facet_wrap(~master_protein_descriptions, nrow = 5, labeller = label_wrap_gen(30)) +
-  scale_y_log10()
-
-
-
-
-# pdf plots ---------------------------------------------------------------
-
-data_plot <- data %>% 
-  ungroup() %>% 
-  select(master_protein_accessions, master_protein_descriptions, peptide_id, 
-         id, estimate) %>% 
-  spread(id, estimate) %>% 
-  gather(id, estimate, 4:11) %>% 
-  separate(id, into = c("tissue", "dev_stage"), remove = FALSE)
-
-prot_list <- data_plot %>% 
-  select(master_protein_accessions, master_protein_descriptions) %>% 
-  distinct() %>% 
-  arrange(master_protein_descriptions)
-
-nrow(prot_list)
-
-prot_list$panel <- rep(c("A","B","C","D","E"), nrow(prot_list)/5)
-prot_list$int <- rep(1:(nrow(prot_list)/5), each = 5)
-
-data_plot <- full_join(prot_list, data_plot)
-
-
-pdf(file = "tissue_turnover_rates.pdf", width = 8.5, height = 11)
-
-for(i in 1:max(prot_list$int)){
-  
-  temp_data <- data_plot %>% filter(int == i)
-  
-  
-  
-  A <- ggplot(temp_data %>% filter(panel == "A")) +
-    geom_boxplot(aes(x = dev_stage, y = estimate), outlier.shape = NA, size = 0.1) +
-    geom_jitter(aes(x = dev_stage, y = estimate), width = 0.1, size = 0.5) +
-    facet_grid(~tissue) +
-    theme_light(base_size = 8) +
-    labs(title = paste(temp_data$master_protein_descriptions[temp_data$panel == "A"][1]),
-         subtitle = paste(temp_data$master_protein_accessions[temp_data$panel == "A"][1])) +
-    scale_y_log10()
-  
-  B <- ggplot(temp_data %>% filter(panel == "B")) +
-    geom_boxplot(aes(x = dev_stage, y = estimate), outlier.shape = NA, size = 0.1) +
-    geom_jitter(aes(x = dev_stage, y = estimate), width = 0.1, size = 0.5) +
-    facet_grid(~tissue) +
-    theme_light(base_size = 8) +
-    labs(title = paste(temp_data$master_protein_descriptions[temp_data$panel == "B"][1]),
-         subtitle = paste(temp_data$master_protein_accessions[temp_data$panel == "B"][1])) +
-    scale_y_log10()
-  
-  C <- ggplot(temp_data %>% filter(panel == "C")) +
-    geom_boxplot(aes(x = dev_stage, y = estimate), outlier.shape = NA, size = 0.1) +
-    geom_jitter(aes(x = dev_stage, y = estimate), width = 0.1, size = 0.5) +
-    facet_grid(~tissue) +
-    theme_light(base_size = 8) +
-    labs(title = paste(temp_data$master_protein_descriptions[temp_data$panel == "C"][1]),
-         subtitle = paste(temp_data$master_protein_accessions[temp_data$panel == "C"][1])) +
-    scale_y_log10()
-  
-  D <- ggplot(temp_data %>% filter(panel == "D")) +
-    geom_boxplot(aes(x = dev_stage, y = estimate), outlier.shape = NA, size = 0.1) +
-    geom_jitter(aes(x = dev_stage, y = estimate), width = 0.1, size = 0.5) +
-    facet_grid(~tissue) +
-    theme_light(base_size = 8) +
-    labs(title = paste(temp_data$master_protein_descriptions[temp_data$panel == "D"][1]),
-         subtitle = paste(temp_data$master_protein_accessions[temp_data$panel == "D"][1])) +
-    scale_y_log10()
-  
-  E <- ggplot(temp_data %>% filter(panel == "E")) +
-    geom_boxplot(aes(x = dev_stage, y = estimate), outlier.shape = NA, size = 0.1) +
-    geom_jitter(aes(x = dev_stage, y = estimate), width = 0.1, size = 0.5) +
-    facet_grid(~tissue) +
-    theme_light(base_size = 8) +
-    labs(title = paste(temp_data$master_protein_descriptions[temp_data$panel == "E"][1]),
-         subtitle = paste(temp_data$master_protein_accessions[temp_data$panel == "E"][1])) +
-    scale_y_log10()
-  
-  
-  
-  print(A + B + C + D + E +
-          plot_layout(ncol = 1))
-  
-  
-}
-
-dev.off()
 
 
 
